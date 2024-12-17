62bb62ac1f2e7b416b3ee2eb35056e80fa659c8b
siftman <ssiftman@gmail.com>
2024-12-17 12:40:29 +0330

    add some basic functions

--- my_plugin.php
+++ my_plugin.php
@@ -1,5 +1,26 @@
 <?php
 /*
-Plugin Name: My Plugin
+Plugin Name: Shopino Plugin
+Plugin URI: http://shopino.app
+Description: افزونه وردپرس شاپینو
+Author: Hossein
+Author URI: http://github.com/siftman
+*/ 
 
-*/
+if (!defined('ABSPATH')){
+    exit;
+}
+
+function my_plugin_footer_text(){
+    echo '<p>Shopino WordPress Plugin';
+}
+
+add_action('wp_footer', 'my_plugin_footer_text')
+
+function my_plugin_activation_notice(){
+    ?>
+    <div class="notice notice-succes is-dimissible">
+        Thanks for using Shopino
+    </div>
+    <?php
+}
